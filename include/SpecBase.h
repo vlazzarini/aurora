b7058bee8139e86acd98c69859971f0506d2e384
Victor Lazzarini <victor.lazzarini@mu.ie>
2022-04-05 18:34:02 +0100

    added phase vocoder

---
+++ include/SpecBase.h
@@ -0,0 +1,200 @@
+// SpecBase.h
+// Spectral Base class
+//
+// (c) V Lazzarini, 2022
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
+// 3. Neither the name of the copyright holder nor the names of its contributors
+// may be used to endorse or promote products derived from this software without
+// specific prior written permission.
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE
+
+#ifndef _AURORA_SPECBASE_
+#define _AURORA_SPECBASE_
+
+#include "SndBase.h"
+#include <complex>
+#include <cmath>
+
+
+namespace Aurora {
+
+  const std::size_t def_fftsize = 1024;
+  const std::size_t def_hsize = 256;
+
+  template <typename S>
+    S unwrap(S ph) { return ph >= M_PI ? ph -twopi : (ph < -M_PI ? ph + twopi : ph); } 
+
+  /** Spectral Data Type \n
+      S: sample type
+   */
+  template <typename S> class specdata {
+    std::complex<S>  bin;
+
+  public:
+
+  /** Spectral Data Type \n
+      default constructor
+   */
+  specdata() { };
+
+   /** Spectral Data Type
+       amp - amplitude \n
+       freq - freq
+   */
+  specdata(S amp, S freq) : bin(amp, freq) {};
+
+   /** Spectral Data Type \n
+       c - complex data in rectangular format
+   */
+  specdata(std::complex<S> c) : bin(c) {
+      S r = bin.real();
+      S i = bin.imag();
+      bin.real(std::sqrt(r*r + i*i));
+      bin.imag(std::atan2(i, r));
+  }
+
+  /** Cast operator \n
+      returns complex data in rectangular format
+  */ 
+  operator std::complex<S> () const {
+    S r = bin.real();
+    S i = bin.imag();
+    return std::complex<S>(r*std::cos(i),r*std::sin(i));
+  }
+
+  /** spectral data amplitude
+   */
+  S amp() const { return bin.real(); }
+
+  /** spectral data frequency
+   */
+  S freq() const { return bin.imag(); }
+
+  /** set spectral data amplitude
+   */
+  void amp(S a) { bin.real(a); }
+
+  /** set spectral data frequency
+   */
+  void freq(S f) { bin.imag(f); }
+
+  /** spectral data amplitude scaling
+   */
+  specdata &operator*=(S amp) {
+      this->bin.real(amp*this->bin.real());
+      return *this;
+    }  
+  
+   /** spectral data amplitude scaling
+   */
+    specdata operator*(S amp) const {
+      specdata res(*this);
+      return res *= amp;
+    }
+
+   /** spectral data product
+   */    
+    friend specdata<S> operator*(S amp, const specdata<S> &d) {
+      return d * amp;
+    }
+
+    /** phase difference \n
+        oph - previous phase \n
+        returns previous phase in object
+    */
+    S diff(S oph) {
+      S ph = bin.imag();
+      bin.imag(unwrap(ph-oph));
+      return ph;
+    }
+
+     /** phase integration \n
+        ph - current phase \n
+        returns current phase in object
+    */
+     S integ(S ph) {
+      bin.imag(ph+bin.imag());
+      return bin.imag();
+    }
+
+     /** radians to Hz conversion \n
+         cf - bin centre frequency in Hz \n
+         fac - fs/(twopi*hopsize) conversion factor \n
+         returns frequency in Hz.
+     */
+    S tocps(S cf, S fac) {
+      return cf + bin.imag()*fac;
+    };
+
+     /** Hz to radian conversion \n
+         cf - bin centre frequency in Hz \n
+         fac - (twopi*hopsize)/fs conversion factor \n
+         returns frequency in Hz.
+     */    
+    S fromcps(S cf, S fac) const {
+      return (bin.imag() - cf)*fac;
+    }
+  
+  };
+
+  /** SpecBase class \n
+     Spectral base class \n
+     S: sample type
+  */  
+  template <typename S>
+   class SpecBase { 
+      std::vector<specdata<S>> spec;
+      std::size_t hs;
+      std::size_t fcnt;
+
+    protected:
+      std::vector<specdata<S>> &get_spec(){ return spec; }
+      void fcount_incr() { fcnt++; }
+
+    public:
+        /** Constructor \n
+        size: spectral frame size
+        hsize: stream hopsize
+        */
+      SpecBase(std::size_t size = def_fftsize, std::size_t hsize = def_hsize) :
+      spec(size/2 + 1), hs(hsize), fcnt(0) { };
+
+      /** spectral frame size 
+       */
+      std::size_t size() const { return (spec.size() - 1)*2; }
+
+      /** spectral hopsize 
+       */
+      std::size_t hsize() const { return hs; }
+
+      /** stream framecount
+       */
+      std::size_t framecount() const { return fcnt; }
+
+      /** Frame access \n
+         returns the object frame
+      */
+      const std::vector<specdata<S>> &frame() const { return spec; }
+  
+  };
+
+}
+
+#endif
---
+++ include/SpecStream.h
@@ -0,0 +1,208 @@
+// SpecStream.h
+// Streaming spectral analysis and synthesis
+//
+// (c) V Lazzarini, 2022
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
+// 3. Neither the name of the copyright holder nor the names of its contributors
+// may be used to endorse or promote products derived from this software without
+// specific prior written permission.
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE
+
+#ifndef _AURORA_SPECSTREAM_
+#define _AURORA_SPECSTREAM_
+
+#include "SpecBase.h"
+#include "FFT.h"
+
+namespace Aurora {
+
+
+  /** SpecStream class \n
+      Spectral stream \n
+      S: sample type
+  */    
+  template <typename S>
+    class SpecStream : public SpecBase<S> {
+    using SpecBase<S>::get_spec;
+    using SpecBase<S>::size;
+    using SpecBase<S>::hsize;
+    using SpecBase<S>::fcount_incr;
+    std::vector<S> buf;
+    std::vector<S> wbuf;
+    std::vector<S> oph;
+    const std::vector<S> &win;
+    FFT<S> fft;
+    S fac, c;
+    std::size_t dm, hnum, pos;
+ 
+    void analysis(const std::vector<S> &in) {
+      std::size_t n = 0;
+      fft.transform(in);
+      auto &v = fft.vector();
+      for(auto &s : get_spec()) {
+	s = v[n];
+	oph[n] = s.diff(oph[n]);
+	s.freq(s.tocps(n*c, fac));
+	n++;
+      }
+    }
+    
+  public:
+    /** Constructor \n
+        window: short-time Fourier transform window \n
+        hsize: stream hopsize \n
+        fs: sampling rate
+    */
+  SpecStream(const std::vector<S> &window, std::size_t hsize = def_hsize, S fs = def_sr) :
+    SpecBase<S>(window.size(), hsize), buf(window.size()), wbuf(window.size()),
+      oph(window.size()/2 + 1), win(window), fft(window.size(), !packed),
+      fac(fs/(twopi*hsize)), c(fs/window.size()), dm(window.size()/hsize),
+      hnum(0), pos(0) { };
+
+    /** Spectral stream analysis \n
+        in: signal input \n
+        returns the current spectral frame
+    */
+    const std::vector<specdata<S>> &operator()(const std::vector<S> &in) {
+      std::size_t vsize = in.size();
+      std::size_t hs = hsize();
+      if(vsize > hs) vsize = hs;
+      std::size_t samps = vsize + pos;
+      if(samps > hs) samps = vsize - samps + hs; 
+      else samps = vsize;
+      std::copy(in.begin(), in.begin() + samps, buf.begin() + pos + hs*hnum);
+      pos += samps;
+      if(pos == hs) {
+        std::size_t n = 0, offs = hs*(dm - hnum - 1);
+	std::size_t N = win.size();
+        for (auto &s : wbuf) {
+	  s = buf[n]*win[(n+offs)%N];
+	  n++;
+	}  
+	analysis(wbuf);
+	pos = 0;
+        hnum = hnum != dm - 1 ? hnum + 1 : 0;
+        if(samps != vsize) { std::copy(in.begin() + samps, in.begin() + vsize,
+				       buf.begin() + hs*hnum);
+          pos += vsize - samps;
+	}
+	fcount_incr();
+      }
+      return get_spec();
+    }
+
+    /** reset the stream parameters \n
+        fs - sampling rate 
+    */ 
+    void reset(S fs) {
+      fac = fs/(twopi*hsize());
+      c = fs/win.size();
+    }
+      
+  };
+
+  /** SpecSynth class \n
+      STFT resynthesis \n
+      S: sample type
+  */    
+  template <typename S>
+    class SpecSynth : public SndBase<S> {
+    using SndBase<S>::get_sig;
+    std::vector<std::vector<S>> buffers;
+    std::vector<std::complex<S>> spec;
+    std::vector<S> ph;
+    const std::vector<S> &win;
+    FFT<S> fft;
+    std::size_t dm, fcnt, pos, hsize, hnum;
+    S fac, c;
+
+    const S *synthesis(const std::vector<specdata<S>> &in) {
+      specdata<S> bin;
+      std::size_t n = 0;
+      for(auto &s : spec) {
+	bin = in[n];
+        bin.freq(bin.fromcps(n*c, fac));
+        ph[n] = bin.integ(ph[n]);
+        s = bin;
+
+	n++;
+      }	
+      return fft.transform(spec);
+    }
+    
+  public:
+
+  /** Constructor \n
+      window: short-time Fourier transform window \n
+      hsize: stream hopsize \n
+      fs: sampling rate \n
+      vsize: output vector size
+  */  
+  SpecSynth(const std::vector<S> &window, std::size_t hsiz = def_hsize,
+	    S fs = def_sr, std::size_t vsize = def_vsize) :
+    SndBase<S>(vsize), buffers(window.size()/hsiz, std::vector<S>(window.size())),
+      spec(window.size()/2 + 1), ph(window.size()/2 + 1), win(window),
+      fft(window.size(), !packed), dm(window.size()/hsiz), fcnt(0),
+      pos(0), hsize(hsiz), hnum(0), fac(twopi*hsiz/fs), c(fs/window.size()) { };
+
+
+    /** Spectral stream synthesis \n
+        in: input spectral frame  \n
+        returns the output vector
+    */    
+    const std::vector<S> &operator() (const std::vector<specdata<S>> &in) {  
+      std::size_t size = win.size();
+      auto &v = get_sig();
+      for(auto &ss : v) {
+	if(++fcnt == hsize) {
+	  std::size_t n = 0;
+	  auto s = synthesis(in);
+	  std::size_t offs =  hsize*hnum;
+	  for(auto &b : buffers[hnum]) {
+	    b = s[(n+offs)%size]*win[n];
+	    n++;
+	  }
+	  hnum = hnum != dm - 1 ? hnum + 1 : 0;
+	  fcnt = 0;
+	}
+	ss = 0.;
+	std::size_t offs = 0;	  
+	for (auto &b : buffers) {
+	  ss += b[(pos-offs+size)%size];
+	  offs += hsize;
+	}
+	pos = pos != size - 1 ? pos + 1 : 0; 
+      }
+      return v;
+    }
+    
+    /** reset the stream parameters \n
+        fs - sampling rate 
+    */ 
+    void reset(S fs) {
+      fac = twopi*hsize/fs;
+      c = fs/win.size();
+    }
+
+  };
+ 
+}
+
+#endif
